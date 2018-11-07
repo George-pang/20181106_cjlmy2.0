76154189062805d5c0119f4ca0ddc8d521c19093
plq <georgepang2016@163.com>
2018-11-07 11:56:37 +0800

    version2.0.2

--- js/course.js
+++ js/course.js
@@ -2,7 +2,22 @@ $(function(){
     $(".tab").on("click",function(){
         $(this).addClass("tab-cur").siblings().removeClass("tab-cur");
         var index=$(this).index();
-        $(".tab-item").eq(index).show().siblings().hide();
+        //在Loop模式下Swiper切换到指定slide。切换到的是slide索引是realIndex
+        mySwiper.slideToLoop(index);
+        // $(".tab-item").eq(index).show().siblings().hide();
     })
 
+    var mySwiper = new Swiper ('.course-swiper', {
+        loop: true, // 循环模式选项
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+          },
+        on:{
+            slideChange: function(){
+                var index=this.realIndex;
+                $(".tab").eq(index).addClass("tab-cur").siblings().removeClass("tab-cur");
+            },
+        }
+    });
 });
